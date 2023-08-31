46629e0b8b83ebb7ebf5697231fce478e0169d8c
mahmoud17tr <54012843+Mahmoud217TR@users.noreply.github.com>
2023-08-31 23:00:08 +0300

    temp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite('resources/sass/app.scss')
 </head>
 <body>
     <div id="app">
@@ -75,6 +75,8 @@
         <main class="py-4">
             @yield('content')
         </main>
+
+        @vite('resources/js/app.js')
     </div>
 </body>
 </html>
